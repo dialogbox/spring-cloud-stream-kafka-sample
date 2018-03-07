bfdda5abb31a7a67248db6dd97e9b21082c66c36
Jason Kim <dialogbox@gmail.com>
2018-03-07 20:38:41 +0900

    spring-kafka

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,37 +1,44 @@
 package com.example.demo;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.messaging.SubscribableChannel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Slf4j
 @SpringBootApplication
-@EnableBinding({CustomSink.class, Sink.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@StreamListener(CustomSink.INPUT)
-	public void processCustomSink(String message) {
-		log.info("Custom: " + message);
+	@Autowired
+	private KafkaTemplate<String, String> template;
+
+	private final CountDownLatch latch = new CountDownLatch(3);
+
+	@Bean
+	public CommandLineRunner cmd() {
+		return args -> {
+			this.template.send("myTopic", "foo1");
+			this.template.send("myTopic", "foo2");
+			this.template.send("myTopic", "foo3");
+			latch.await(60, TimeUnit.SECONDS);
+			log.info("All received");
+		};
 	}
 
-	@StreamListener(Sink.INPUT)
-	public void processSink(String message) {
-		log.info("Normal: " + message);
+	@KafkaListener(topics = "myTopic")
+	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+		log.info(cr.toString());
+		latch.countDown();
 	}
-}
-
-interface CustomSink {
-	String INPUT = "myInputTopic";
-
-	@Input(CustomSink.INPUT)
-	SubscribableChannel input();
 }
